5afbb49f9e11151b94a764902f568b735ac65507
Javan Graham <javanag8@gmail.com>
2021-01-11 19:30:09 -0500

    Make username not nullable for User

---
+++ db/migrate/20210112002549_change_username_not_null_user.rb
@@ -0,0 +1,5 @@
+class ChangeUsernameNotNullUser < ActiveRecord::Migration[6.1]
+  def change
+    change_column_null :users, :username, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_234622) do
+ActiveRecord::Schema.define(version: 2021_01_12_002549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 2021_01_11_234622) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "username"
+    t.string "username", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
