85b2cabcf7822a02e5103e080d31a3a02aaa1101
Javan Graham <javanag8@gmail.com>
2021-01-14 20:55:08 -0500

    Display message when surprising with no images

--- app/controllers/surprise_controller.rb
+++ app/controllers/surprise_controller.rb
@@ -1,5 +1,10 @@
 class SurpriseController < ApplicationController
   def index
-    redirect_to image_path(Image.find(Image.open.pluck(:id).sample))
+    if Image.count > 0
+      redirect_to image_path(Image.find(Image.open.pluck(:id).sample))
+    else
+      flash[:notice] = "There are no open images to view!"
+      redirect_to images_path
+    end
   end
 end
